20fb5e21c8786fb98619694de3ecf9130465015d
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-05-27 01:50:15 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js'
+  },
+  devServer: {
+    allowedHosts: [
+      'all'
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/styles.css', to: './' } // output path is 'public' folder, already defined.
+      ]
+    })
+  ]
+}

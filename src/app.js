2766e23d7dc922ce481624ab5697f7820c60ad11
AnandRaja-tech <anand.official.in@gmail.com>
2022-05-27 19:00:02 +0000

    basic xml http req

--- src/app.js
+++ src/app.js
@@ -27,15 +27,39 @@ app.controller('mainController', ['$scope', '$log', '$filter', '$timeout', funct
 
   $scope.charaters = 5;
 
-  $scope.rules = [
-    {ruleName: 'Must be 5 characters'},
-    {ruleName: 'Must not be used else where'},
-    {ruleName: 'Must be cool'}
-  ];
+  // $scope.rules = [
+  //   {RuleName: 'Must be 5 characters'},
+  //   {RuleName: 'Must not be used else where'},
+  //   {RuleName: 'Must be cool'}
+  // ];
 
   $scope.alertClick = function() {
     alert('You clicked me!');
   }
+
+  // XML http req
+  // if result didn't arrive, use this code (with $apply)
+  
+  // rulesReq.onreadystatechange = function () {
+  //   $scope.$apply(function () {
+  //     if (rulesReq.readyState == 4 && rulesReq.status == 200) {
+  //       $scope.rules = JSON.parse(rulesReq.responseText);
+  //     }
+  //   });
+  // }
+
+  var rulesReq = new XMLHttpRequest();
+  var url = 'https://raw.githubusercontent.com/actionanand/json-server/main/db/api/angular-js/rough-work/rules.json';
+
+
+  rulesReq.onreadystatechange = function () {
+    if (rulesReq.readyState == 4 && rulesReq.status == 200) {
+      $scope.rules = JSON.parse(rulesReq.responseText);
+    }
+  }
+
+  rulesReq.open("GET", url, true);
+  rulesReq.send();
 
 }]);
 

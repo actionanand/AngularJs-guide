7d8bebcef948a10d2bc973836dfe06c5a2dbecfc
AnandRaja-tech <anand.official.in@gmail.com>
2022-05-27 12:04:19 +0000

    DI and minification

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 // MODULE
-var app = angular.module('angularApp', []);
+var app = angular.module('angularApp', ['ngMessages']);
 
 // CONTROLLERS
-app.controller('mainController', ['$scope', function ($scope) {
-    console.log($scope);
+app.controller('mainController', ['$scope', '$log', '$filter', function ($scope, $log, $filter) {
+  $scope.name = 'Anand Raja';
+  console.log($scope);
+  $scope.filteredText = $filter('uppercase')($scope.name);
+  $log.warn($scope.filteredText);
 }]);
+
+var searchPeople = function(firstName, lastName, Height, age, occupation) {
+  return 'Anand Raja';
+};
+
+console.log(angular.injector().annotate(searchPeople));
